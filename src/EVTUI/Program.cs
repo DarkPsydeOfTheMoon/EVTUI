1d928a60101c41b2411682fccaabaea28e2cbb45
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2024-05-26 17:56:57 -0700

    Prevent multiple app instances from running.

--- src/EVTUI/Program.cs
+++ src/EVTUI/Program.cs
@@ -2,17 +2,26 @@
 using Avalonia.ReactiveUI;
 using System;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace EVTUI;
 
 sealed class Program
 {
+    private static string AppName = "Global\\EVTUI";
+    private static Mutex AppMutex = new Mutex(initiallyOwned: true, AppName);
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        if (AppMutex.WaitOne(TimeSpan.Zero, true))
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        else
+            Console.WriteLine("EVTUI instance already open.");
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
