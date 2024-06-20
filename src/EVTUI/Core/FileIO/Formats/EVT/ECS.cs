48db84a734031b0fe19f68b2febf418130fd362e
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2024-06-20 13:14:57 -0700

    EVT and ECS code cleanup.

--- src/EVTUI/Core/FileIO/Formats/EVT/ECS.cs
+++ src/EVTUI/Core/FileIO/Formats/EVT/ECS.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 using Serialization;
 
@@ -11,53 +12,49 @@ public class ECS : ISerializable
     private static Int32 ENTRY_OFFSET = 16;
     private static Int32 RESERVE      = 0;
 
-    public string Endianness;
-    public Int32 CommandCount;
+    public bool   IsLittleEndian;
+    public Int32  CommandCount;
     public UInt32 CommandOffset;
     public UInt32 CommandSize;
     public UInt32 Reserve;
+
     public SerialCommand[] Commands;
-    public ArrayList CommandData;
+    public ArrayList       CommandData;
 
     public void ExbipHook<T>(T rw, Dictionary<string, object> args) where T : struct, IBaseBinaryTarget
     {
         if (rw.IsConstructlike())
         {
-            this.Endianness = "big";
-            rw.SetEndianness(this.Endianness);
+            this.IsLittleEndian = false;
+            rw.SetLittleEndian(this.IsLittleEndian);
             rw.RwInt32(ref this.CommandCount);
             rw.RwUInt32(ref this.CommandOffset);
             rw.RwUInt32(ref this.CommandSize);
             if (this.CommandSize != EVT.ENTRY_SIZE)
             {
-                this.Endianness = "little";
-                rw.SetEndianness(this.Endianness);
+                this.IsLittleEndian = true;
+                rw.SetLittleEndian(this.IsLittleEndian);
             }
             rw.Seek(0, 0);
         }
         else if (rw.IsParselike())
-            rw.SetEndianness(this.Endianness);
+            rw.SetLittleEndian(this.IsLittleEndian);
 
         rw.RwInt32(ref this.CommandCount);
 
         rw.RwUInt32(ref this.CommandOffset);
-        if (this.CommandOffset != ECS.ENTRY_OFFSET)
-            throw new Exception($"ECS command offset ({this.CommandOffset}) does not match expected offset ({ECS.ENTRY_OFFSET})");
+        Trace.Assert(this.CommandOffset == ECS.ENTRY_OFFSET, $"ECS command offset ({this.CommandOffset}) does not match expected offset ({ECS.ENTRY_OFFSET})");
 
         rw.RwUInt32(ref this.CommandSize);
-        if (this.CommandSize != EVT.ENTRY_SIZE)
-            throw new Exception($"ECS command size ({this.CommandSize}) does not match expected offset ({EVT.ENTRY_SIZE})");
+        Trace.Assert(this.CommandSize == EVT.ENTRY_SIZE, $"ECS command size ({this.CommandSize}) does not match expected offset ({EVT.ENTRY_SIZE})");
 
         rw.RwUInt32(ref this.Reserve);
-        if (this.Reserve != ECS.RESERVE)
-            throw new Exception($"ECS reserve ({this.Reserve}) does not match expected value ({ECS.RESERVE})");
+        Trace.Assert(this.Reserve == ECS.RESERVE, $"ECS reserve ({this.Reserve}) does not match expected value ({ECS.RESERVE})");
 
-        if (rw.Tell() != this.CommandOffset)
-            throw new Exception($"Stream position should be at end of ECS header ({this.CommandOffset}) but is instead at position {rw.Tell()}");
+        Trace.Assert(rw.Tell() == this.CommandOffset, $"Stream position should be at end of ECS header ({this.CommandOffset}) but is instead at position {rw.Tell()}");
 
         rw.RwObjs(ref this.Commands, this.CommandCount);
-        if (this.Commands.Length != this.CommandCount)
-            throw new Exception($"Number of commands ({this.Commands.Length}) doesn't match expected CommandCount ({this.CommandCount})");
+        Trace.Assert(this.Commands.Length == this.CommandCount, $"Number of commands ({this.Commands.Length}) doesn't match expected CommandCount ({this.CommandCount})");
 
         if (rw.IsConstructlike())
            this.CommandData = new ArrayList();
