69398e57081b83d78b82a3f723386a5c6c44d386
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2025-10-31 14:32:36 -0700

    One last set of awaits, lol.

--- src/gui/EditorWindow/EditorWindow.axaml.cs
+++ src/gui/EditorWindow/EditorWindow.axaml.cs
@@ -23,7 +23,7 @@ public partial class EditorWindow : Window
     {
         try
         {
-            ((EditorWindowViewModel)DataContext).SaveMod(((MenuItem)sender).Name);
+            await ((EditorWindowViewModel)DataContext).SaveMod(((MenuItem)sender).Name);
             await Utils.RaiseModal(this, "Saved successfully!");
         }
         catch (IOException ex)
