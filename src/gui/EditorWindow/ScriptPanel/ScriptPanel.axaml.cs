6a4e01e779fe247ec36ab24a32e45cc32d3b6a66
DarkPsydeOfTheMoon <avalanchebreakdown@gmail.com>
2025-12-04 17:51:35 -0800

    Further script panel optimizations to prevent the need to reload the grammars a bunch...?

--- src/gui/EditorWindow/ScriptPanel/ScriptPanel.axaml.cs
+++ src/gui/EditorWindow/ScriptPanel/ScriptPanel.axaml.cs
@@ -21,12 +21,23 @@ namespace EVTUI.Views;
 public partial class ScriptPanel : ReactiveUserControl<ScriptPanelViewModel>
 {
     private Window topLevel;
-    private TextEditor _textEditor;
-    private TextMate.Installation _textMateInstallation;
-
+    //private TextEditor _textEditor;
+    //private TextMate.Installation _textMateInstallation;
     private static RegistryOptions _registryOptions = new RegistryOptions(ThemeName.Monokai);
+
+    private TextEditor            _msgTextEditor;
+    private TextMate.Installation _msgTextMateInstallation;
     private static string _jsonGrammar = _registryOptions.GetScopeByLanguageId("json");
-    private string _cppGrammar = _registryOptions.GetScopeByLanguageId("cpp");
+
+    private TextEditor            _flowTextEditor;
+    private TextMate.Installation _flowTextMateInstallation;
+    private static string _cppGrammar = _registryOptions.GetScopeByLanguageId("cpp");
+
+    private IDisposable _subscription;
+
+    //private static RegistryOptions _registryOptions = new RegistryOptions(ThemeName.Monokai);
+    //private static string _jsonGrammar = _registryOptions.GetScopeByLanguageId("json");
+    //private string _cppGrammar = _registryOptions.GetScopeByLanguageId("cpp");
 
     public ScriptPanel()
     {
@@ -37,10 +48,12 @@ public partial class ScriptPanel : ReactiveUserControl<ScriptPanelViewModel>
             if (tl is null) throw new NullReferenceException();
             this.topLevel = (Window)tl;
 
-            if (_textEditor is null)
+            //if (_textEditor is null)
+            if (_msgTextEditor is null || _flowTextEditor is null)
             {
                 this.InitializeTextEditor();
                 this.UpdateTextEditor();
+                //this.WhenAnyValue(x => x._textEditor.Document.Text).Subscribe(x => ViewModel!.SelectedScriptContent = x );
             }
         });
     }
@@ -49,7 +62,8 @@ public partial class ScriptPanel : ReactiveUserControl<ScriptPanelViewModel>
     {
         try
         {
-            if (!(_textEditor is null))
+            //if (!(_textEditor is null))
+            if (!(EditorContainer.Content is null))
             {
                 ViewModel!.SelectedCompiledScriptName = (string)(CompiledName.SelectedItem);
                 ViewModel!.UpdateSubfiles();
@@ -68,7 +82,8 @@ public partial class ScriptPanel : ReactiveUserControl<ScriptPanelViewModel>
     {
         try
         {
-            if (!(_textEditor is null))
+            //if (!(_textEditor is null))
+            if (!(EditorContainer.Content is null))
                 this.UpdateTextEditor();
         }
         catch (Exception ex)
@@ -93,22 +108,65 @@ public partial class ScriptPanel : ReactiveUserControl<ScriptPanelViewModel>
 
     public void InitializeTextEditor()
     {
-        _textEditor = new TextEditor();
+        //_textEditor = new TextEditor();
+        //// very goofy to have to do this here but I can't figure out the right XAML for it lolz
+        //_textEditor.TextArea.TextView.Margin = new Thickness(0, 0, 20, 0);
+        //_textEditor.IsReadOnly = !(ViewModel!.Editable);
+        //if (!(ViewModel!.Editable))
+        //    _textEditor.TextArea.Caret.CaretBrush = Brushes.Transparent;
+        //_textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
+        //_textEditor.Document = new TextDocument("");
+        //EditorContainer.Content = _textEditor;
+
+        _msgTextEditor = new TextEditor();
         // very goofy to have to do this here but I can't figure out the right XAML for it lolz
-        _textEditor.TextArea.TextView.Margin = new Thickness(0, 0, 20, 0);
-        _textEditor.IsReadOnly = !(ViewModel!.Editable);
+        _msgTextEditor.TextArea.TextView.Margin = new Thickness(0, 0, 20, 0);
+        _msgTextEditor.IsReadOnly = !(ViewModel!.Editable);
         if (!(ViewModel!.Editable))
-            _textEditor.TextArea.Caret.CaretBrush = Brushes.Transparent;
-        _textMateInstallation = _textEditor.InstallTextMate(_registryOptions);
-        EditorContainer.Content = _textEditor;
+            _msgTextEditor.TextArea.Caret.CaretBrush = Brushes.Transparent;
+        _msgTextMateInstallation = _msgTextEditor.InstallTextMate(_registryOptions);
+        _msgTextMateInstallation.SetGrammar(_jsonGrammar);
+        _msgTextEditor.Document = new TextDocument("");
+
+        _flowTextEditor = new TextEditor();
+        // very goofy to have to do this here but I can't figure out the right XAML for it lolz
+        _flowTextEditor.TextArea.TextView.Margin = new Thickness(0, 0, 20, 0);
+        _flowTextEditor.IsReadOnly = !(ViewModel!.Editable);
+        if (!(ViewModel!.Editable))
+            _flowTextEditor.TextArea.Caret.CaretBrush = Brushes.Transparent;
+        _flowTextMateInstallation = _flowTextEditor.InstallTextMate(_registryOptions);
+        _flowTextMateInstallation.SetGrammar(_cppGrammar);
+        _flowTextEditor.Document = new TextDocument("");
+
+        //EditorContainer.Content = _textEditor;
     }
 
     public void UpdateTextEditor()
     {
-        _textEditor.IsEnabled = ViewModel!.HasDecompiledFiles;
-        _textEditor.Document = new TextDocument(ViewModel!.SelectedScriptContent);
-        this.WhenAnyValue(x => x._textEditor.Document.Text).Subscribe(x => ViewModel!.SelectedScriptContent = x );
-        if (ViewModel!.HasDecompiledFiles)
-            _textMateInstallation.SetGrammar((ViewModel!.SelectedDecompiledScriptName.EndsWith(".msg")) ? _jsonGrammar : _cppGrammar);
+        //_textEditor.IsEnabled = ViewModel!.HasDecompiledFiles;
+        ////_textEditor.Document = new TextDocument(ViewModel!.SelectedScriptContent);
+        ////this.WhenAnyValue(x => x._textEditor.Document.Text).Subscribe(x => ViewModel!.SelectedScriptContent = x );
+        //_textEditor.Document.Text = ViewModel!.SelectedScriptContent;
+        //if (ViewModel!.HasDecompiledFiles)
+        //    _textMateInstallation.SetGrammar((ViewModel!.SelectedDecompiledScriptName.EndsWith(".msg")) ? _jsonGrammar : _cppGrammar);
+
+        if (!(_subscription is null))
+            _subscription.Dispose();
+
+        //if (ViewModel!.IsMsg[ViewModel!.SelectedCompiledScriptName])
+        if (ViewModel!.SelectedDecompiledScriptName.EndsWith(".msg"))
+        {
+            _msgTextEditor.IsEnabled = ViewModel!.HasDecompiledFiles;
+            _msgTextEditor.Document.Text = ViewModel!.SelectedScriptContent;
+            EditorContainer.Content = _msgTextEditor;
+            _subscription = this.WhenAnyValue(x => x._msgTextEditor.Document.Text).Subscribe(x => ViewModel!.SelectedScriptContent = x );
+        }
+        else
+        {
+            _flowTextEditor.IsEnabled = ViewModel!.HasDecompiledFiles;
+            _flowTextEditor.Document.Text = ViewModel!.SelectedScriptContent;
+            EditorContainer.Content = _flowTextEditor;
+            _subscription = this.WhenAnyValue(x => x._flowTextEditor.Document.Text).Subscribe(x => ViewModel!.SelectedScriptContent = x );
+        }
     }
 }
